66e6ed9b068428bc3a5f0ddbe45d4a623b77b71e
Fyvel <fdelage8000@gmail.com>
2021-02-18 17:04:43 +1000

    ➕🧪 Add unit tests - ReleaseRetention.spec.ts

--- src/tests/ReleaseRetention.spec.ts
+++ src/tests/ReleaseRetention.spec.ts
@@ -11,22 +11,139 @@ describe('ReleaseRetention module', () => {
 
 describe('ReleaseRetention factory', () => {
 	test('Should identify relevant releases to process', () => {
-		// Arrange
+		//#region Arrange
+		const targetProjects = [
+			{ Id: 'Project-A' },
+			{ Id: 'Project-B' },
+			{ Id: 'Project-C' }
+		] as Project[]
+		const releaseArray = [
+			{
+				Id: 'R1',
+				ProjectId: 'Project-A'
+			},
+			{
+				Id: 'R2',
+				ProjectId: 'Project-B'
+			},
+			{
+				Id: 'R3',
+				ProjectId: 'Project-D'
+			},
+			{
+				Id: 'R4',
+				ProjectId: 'Project-E'
+			}] as Release[]
+		const expected = [{
+			Id: 'R1',
+			ProjectId: 'Project-A'
+		},
+		{
+			Id: 'R2',
+			ProjectId: 'Project-B'
+		}] as Release[]
+		//#endregion
 		// Act
+		const results = findReleasesToProcess(releaseArray, targetProjects)
 		// Assert
-		expect(false).toBe(true)
+		expect(results.length).toBe(expected.length)
+		expect(results).toEqual(expect.arrayContaining(expected))
 	})
 	test('Should identify relevant deployments', () => {
-		// Arrange
+		//#region Arrange
+		const targetEnvironments = [
+			{ Id: 'Env-A' },
+			{ Id: 'Env-B' },
+			{ Id: 'Env-C' }
+		] as Environment[]
+		const deploymentArray = [
+			{
+				Id: 'D1',
+				EnvironmentId: 'Env-A'
+			},
+			{
+				Id: 'D2',
+				EnvironmentId: 'Env-B'
+			},
+			{
+				Id: 'D3',
+				EnvironmentId: 'Env-D'
+			},
+			{
+				Id: 'D4',
+				EnvironmentId: 'Env-E'
+			}] as Deployment[]
+		const expected = [{
+			Id: 'D1',
+			EnvironmentId: 'Env-A'
+		},
+		{
+			Id: 'D2',
+			EnvironmentId: 'Env-B'
+		}] as Deployment[]
+		//#endregion
 		// Act
+		const results = findDeploymentsToProcess(deploymentArray, targetEnvironments)
 		// Assert
-		expect(false).toBe(true)
+		expect(results.length).toBe(expected.length)
+		expect(results).toEqual(expect.arrayContaining(expected))
 	})
 	test('Should find Project/Environemnt combinaisons', () => {
-		// Arrange
+		//#region Arrange
+		const releaseArray = [
+			{
+				Id: 'R1',
+				ProjectId: 'Project-A'
+			},
+			{
+				Id: 'R2',
+				ProjectId: 'Project-B'
+			},
+			{
+				Id: 'R3',
+				ProjectId: 'Project-A'
+			},
+			{
+				Id: 'R4',
+				ProjectId: 'Project-B'
+			},
+			{
+				Id: 'R5',
+				ProjectId: 'Project-D'
+			}
+		] as Release[]
+		const deploymentArray = [
+			{
+				Id: 'D1',
+				EnvironmentId: 'Env-A',
+				ReleaseId: 'R1' // 'Project-A'
+			},
+			{
+				Id: 'D2',
+				EnvironmentId: 'Env-B',
+				ReleaseId: 'R2' // 'Project-B'
+			},
+			{
+				Id: 'D3',
+				EnvironmentId: 'Env-D',
+				ReleaseId: 'R3' // 'Project-A'
+			},
+			{
+				Id: 'D4',
+				EnvironmentId: 'Env-E',
+				ReleaseId: 'R4' //'Project-B'
+			}] as Deployment[]
+		const expected = new Map()
+			.set('Project-A_Env-A', {})
+			.set('Project-A_Env-D', {})
+			.set('Project-B_Env-B', {})
+			.set('Project-B_Env-E', {}) as ProjectEnvironment
+		//#endregion
 		// Act
+		const result = groupByProjectEnvironment(releaseArray, deploymentArray)
 		// Assert
-		expect(false).toBe(true)
+		expect(result.size).toBe(expected.size)
+		expect([...result.keys()]).toEqual(expect.arrayContaining([...expected.keys()]))
 	})
 })
 
