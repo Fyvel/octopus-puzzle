e9c8abe6285313019406486f781522c574a3fbe8
Fyvel <fdelage8000@gmail.com>
2021-02-22 19:51:28 +1000

    ⏲️ Add timer

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 'use strict'
 
+import { performance } from 'perf_hooks'
+
 import ReleaseRetention from './ReleaseRetention'
 import Data from './data'
 
 const numberOfRelease = 10
 
+const start = performance.now()
 const releasesKept = ReleaseRetention.Create(Data).Keep(numberOfRelease)
+const end = performance.now()
 
-console.log(JSON.stringify(releasesKept, null, 2))
+console.info(JSON.stringify(`${releasesKept}`, null, 2))
+console.log(`Last for ${end - start} ms`)
